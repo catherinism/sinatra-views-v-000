4ba6431e1d5c1ab2d50565e0c845d3f8683a1bee
Catherine Salazar <catcreates13@gmail.com>
2018-10-01 02:15:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		 erb :index
-	 end
-
-	 get "/info" do
-		 "Testing the info page"
-	 end
+    "<h1>Hello World</h1>"
+end
 
 end
